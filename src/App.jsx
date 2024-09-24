7faab7dcbc5cf634151d19c8318f271ff2e328e3
muhammed salih <muhammadsalih563@gmail.com>
2024-09-24 10:37:14 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import LoginRegister from './Components/LoginRegister/LoginRegister'
+
+function App() {
+  return (
+    <div>
+      <LoginRegister/>
+    </div>
+  )
+}
+
+export default App
